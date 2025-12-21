8a32f37f182c9690ee5a890a5805e4bd043bec78
SirojbekMuxtorov2006 <sirojbekmuxtorov9@gmail.com>
2025-12-21 15:07:16 +0500

    telegram clone start

---
+++ client/app/layout.tsx
@@ -0,0 +1,36 @@
+import './globals.css';
+
+import type { Metadata } from 'next';
+import { Space_Grotesk } from 'next/font/google';
+
+import { Toaster } from '@/components/ui/toaster';
+
+const spaceGrotesk = Space_Grotesk({
+	weight: ['400', '500', '600', '700', '300'],
+	subsets: ['latin'],
+	variable: '--font-spaceGrotesk',
+});
+
+export const metadata: Metadata = {
+	title: 'Telegram web',
+	description: 'Telegram web application clone created by Sammi.ac',
+	icons: { icon: '/logo.svg' },
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang='en' suppressHydrationWarning>
+			<body
+				className={`${spaceGrotesk.variable} antialiased sidebar-custom-scrollbar`}
+				suppressHydrationWarning
+			>
+				<main>{children}</main>
+				<Toaster />
+			</body>
+		</html>
+	);
+}
